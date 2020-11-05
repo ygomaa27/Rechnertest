dccd9728bec4ff3ed9d6922e93cabe1e04fbf269
Yasin <wi19b072@technikum-wien.at>
2020-11-05 10:04:03 +0100

    Test improved

---
+++ XUnitTestProject2/RechnerFurKreisfunktionenSteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    [Binding]
+    public class RechnerFurKreisfunktionenSteps
+    {
+
+
+        double num, res;
+        [Given(@"the number is PI")]
+        public void GivenTheNumberIsPI()
+        {
+            num = Math.PI;
+        }
+
+        [When(@"the number use the Sinus function")]
+        public void WhenTheNumberUseTheSinusFunction()
+        {
+            res = Math.Sin(num);
+        }
+
+        [When(@"the number use the Cosinus function")]
+        public void WhenTheNumberUseTheCosinusFunction()
+        {
+            res = Math.Cos(num);
+        }
+
+        [When(@"the number use the Tangenss function")]
+        public void WhenTheNumberUseTheTangenssFunction()
+        {
+            res = Math.Tan(num);
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(double p0)
+        {
+            Assert.Equal(p0, res, 5);
+        }
+    }
+}
+
---
+++ XUnitTestProject2/SpecFlowFeature1.feature
@@ -0,0 +1,17 @@
+﻿Feature:Rechner für Kreisfunktionen
+
+@mytag
+Scenario: Sinus
+	Given the number is PI
+	When the number use the Sinus function
+	Then the result should be 0
+
+Scenario: Cosinus
+	Given the number is PI
+	When the number use the Cosinus function
+	Then the result should be -1
+
+Scenario: Tangens
+	Given the number is PI 
+	When the number use the Tangenss function
+	Then the result should be 0
